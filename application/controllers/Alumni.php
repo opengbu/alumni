7e8d11f31b92e057daf38251761d098f15dab4be
varun-garg <varun.10@live.com>
2015-10-08 23:23:25 +0000

    Remove unrequired files

--- application/controllers/Alumni.php
+++ application/controllers/Alumni.php
@@ -1,131 +1,29 @@
 <?php
 
 /*  Note : class="active" in navbar is dynamic
- 	home=1
-	distinguished=2
-	alumnilist=3
-	events=4
-	gallery=5
-	profile=6
-*/
+  home=1
+  distinguished=2
+  alumnilist=3
+  events=4
+  gallery=5
+  profile=6
+ */
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Alumni extends CI_Controller {
 
-     public function index()
-	{
-		$data['title'] = 'Welcome to Gbu Alumni';
-		$this->load->view('pages/common/link',$data);
-		$this->load->view('pages/common/header');
-		$this->load->view('pages/alumni/home');
-        $this->load->view('pages/common/footer');
-		$this->load->helper('date');
-    }
-	
-	
-    public function name() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
+    public function index() {
+        $data['title'] = 'Welcome to Gbu Alumni';
         $this->load->view('pages/common/link', $data);
         $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/alumniList');
+        $this->load->view('pages/alumni/home');
         $this->load->view('pages/common/footer');
-    }
-    public function cname() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/cname');
-        $this->load->view('pages/common/footer');
-    }
-    public function company() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/company');
-        $this->load->view('pages/common/footer');
+        $this->load->helper('date');
     }
 
-    public function location() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/location');
-        $this->load->view('pages/common/footer');
-    }
-    
-	public function d_name() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/d_name');
-        $this->load->view('pages/common/footer');
-    }
-	 public function d_cname() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/d_cname');
-        $this->load->view('pages/common/footer');
-    }
-    public function d_company() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List ';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/d_company');
-        $this->load->view('pages/common/footer');
-    }
-
-    public function d_location() {
-        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
-        $data['heading'] = ' Alumni List';
-        $data['message'] = 'Let the world know you.....';
-		$data['x'] = 3;
-        $this->load->view('pages/common/link', $data);
-        $this->load->view('pages/common/header');
-        $this->load->view('pages/common/page-heading', $data);
-		$this->load->view('pages/alumni/nav', $data);
-        $this->load->view('pages/alumni/d_location');
-        $this->load->view('pages/common/footer');
-    }
-	
     public function events() {
-        
+
         $this->load->view('pages/common/link');
         $this->load->view('pages/common/header');
         $this->load->view('pages/alumni/events');
@@ -133,11 +31,11 @@ class Alumni extends CI_Controller {
     }
 
     public function gallery() {
-       
+
         $this->load->view('pages/common/link');
         $this->load->view('pages/common/header');
         $this->load->view('pages/alumni/gallery');
         $this->load->view('pages/common/footer');
     }
-	
+
 }
