6e908bd65a615107b850b3066ab191f683d58adc
varun-garg <varun.10@live.com>
2015-10-09 20:09:00 +0000

    Improved gallery looked, plus fixed footer for real

--- application/views/pages/alumni/gallery.php
+++ application/views/pages/alumni/gallery.php
@@ -10,18 +10,13 @@ echo '<br /><br />'
     <div class="popup-gallery">
         <?php
         foreach ($names as $name) {
-            echo '<a href ="' . base_url('user_uploads/gallery/' . $name) . '" ><img src = "' . base_url('user_uploads/gallery/' . $name) . '" height="300" width="300"/></a>';
+            echo '<div class="col-sm-4">';
+            echo '<a href ="' . base_url('user_uploads/gallery/' . $name) . '" ><img src = "' . base_url('user_uploads/gallery/' . $name) . '" width="100%"/></a>';
+            echo '</div>';
         }
         ?>
-        <!--
-        <a href="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_b.jpg" ><img src="http://farm9.staticflickr.com/8242/8558295633_f34a55c1c6_s.jpg"></a>
-        <a href="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_b.jpg" ><img src="http://farm9.staticflickr.com/8382/8558295631_0f56c1284f_s.jpg" height="75" width="75"></a>
-        <a href="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg" ><img src="http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_s.jpg" height="75" width="75"></a>
-        <a href="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_b.jpg" ><img src="http://farm9.staticflickr.com/8383/8563475581_df05e9906d_s.jpg" height="75" width="75"></a>
-        <a href="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_b.jpg" ><img src="http://farm9.staticflickr.com/8235/8559402846_8b7f82e05d_s.jpg" height="75" width="75"></a>
-        <a href="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg" ><img src="http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_s.jpg" height="75" width="75"></a>
-        <a href="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_b.jpg" ><img src="http://farm9.staticflickr.com/8378/8559402848_9fcd90d20b_s.jpg" height="75" width="75"></a>
-        -->
+            <br /><br /><br /><br /><br /><br /><br />
+
     </div>
 </div>
 <script>
--- application/views/pages/common/footer.php
+++ application/views/pages/common/footer.php
@@ -1,53 +1,54 @@
-	</div> <!--for container fluid opened in link.php-->
+</div> <!--for container fluid opened in link.php-->
 
-    <style>
+<style>
 
-        footer {
-            margin: 0px;
-            padding-left: 270px; 
-            width : 100%;
-			position: absolute; 
-			z-index: 100; 
-			background-color: #2D2D2D;
-        }
-    </style>
+    footer {
+        margin: 0px;
+        padding-left: 270px; 
+        width : 100%;
+        position: absolute; 
+        z-index: 100; 
+        background-color: #2D2D2D;
+    }
+</style>
 
 
-    <footer class="footer container-fluid"  role="contentinfo">
-        <br>
-              
-			
-            <div class="col-md-12 text-center">
-                <br/>
-                    <b><p style = "color:white;"><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> OpenGBU, Inc.</p></b>
-              <br/>
-       </div>
-        
+<footer class="footer container-fluid"  role="contentinfo">
+    <br>
 
-    </footer>
+
+    <div class="col-md-12 text-center">
+        <br/>
+        <b><p style = "color:white;"><span class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></span> OpenGBU, Inc.</p></b>
+        <br/>
+    </div>
+
+
+</footer>
 
 
 <!--disqus code for counting comments begins-->
 <script type="text/javascript">
-        
-        
-	if(count_comments==true)
-	{
-		/* * * CONFIGURATION VARIABLES * * */
-		var disqus_shortname = 'gbuonline';
-	//	var disqus_identifier = 'blog_7';
-	
-		/* * * DON'T EDIT BELOW THIS LINE * * */
-		(function () {
-			var s = document.createElement('script'); s.async = true;
-			s.type = 'text/javascript';
-			s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-			(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-		}());
-	}
+
+
+    if (count_comments == true)
+    {
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'gbuonline';
+        //	var disqus_identifier = 'blog_7';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function () {
+            var s = document.createElement('script');
+            s.async = true;
+            s.type = 'text/javascript';
+            s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+            (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+        }());
+    }
 </script>
 <!--disqus code for counting comments ends-->
-	
-	
+
+
 </body>
 </html>
