6251c68b4868c8914bf42122d62f9fca11fcefdc
Rajat Saxena <rajat.saxena035@gmail.com>
2015-10-10 06:54:34 +0530

    alumni_list

--- application/views/pages/alumni/all_alumni.php
+++ application/views/pages/alumni/all_alumni.php
@@ -23,31 +23,36 @@
     <?php
 	if($special=='all')
 	{
-		$select_rows = $this->db->query("select full_name, roll_number, users.user_id, gender from users;");
+		$select_rows = $this->db->query("select full_name, user_id from users;");
     }
 	else if($special=='distinguished') 
 	{
-		$select_rows = $this->db->query("select full_name, roll_number, users.user_id, gender from users where distinguished = '1';");
+		$select_rows = $this->db->query("select full_name, user_id from users where distinguished = '1';");
 	}
 	$sum = 0;
-
+	
     if ($select_rows->num_rows() > 0) {
         ?>
         <ul class="nav nav-list col-sm-12" id = "record_list"> 
             <b>
                 <li class="list-group-item">
                     <div class="row">
-                        <div class="col-sm-4">
+                        <div class="col-sm-3">
                             Name
                         </div>
-                        <div class="col-sm-2">
-                            Sex
+						<div class="col-sm-2">
+                            School
                         </div>
-                        <div class="col-sm-3">
-                            Roll Number
+                        <div class="col-sm-1">
+                            Passout
                         </div>
-
-                        <div class="col-sm-3">
+						<div class="col-sm-3">
+                            Organization
+                        </div>
+						<div class="col-sm-2">
+                            Designation
+                        </div>
+                        <div class="col-sm-1">
                             Actions
                         </div>
                     </div>
@@ -56,7 +61,11 @@
             <?php
 			$count=0;
             foreach ($select_rows->result() as $row) {
-                $count++;
+				$edu_q = $this->db->query("select passout_year, school_name from edu_info where user_id='$row->user_id' and passout_year IN (select max(passout_year) from edu_info where user_id = '$row->user_id');");
+				$edu = $edu_q->row();
+				$work_q = $this->db->query("select company_name, designation from work_details where user_id='$row->user_id' and start_year IN (select max(start_year) from work_details where user_id = '$row->user_id');");
+                $work = $work_q->row();
+				$count++;
 				if($count%2==0)
 				{
 					$color='default';
@@ -68,16 +77,22 @@
 				?>
                 <li class="list-group-item list-group-item-<?=$color?>">
                     <div class="row">
-                        <div class="col-sm-4">
-                            <?= $row->full_name ?>
+						<div class="col-sm-3">
+							<?= $row->full_name ?>
                         </div>
-                        <div class="col-sm-2">
-                            <?= ucfirst($row->gender) ?>
+						<div class="col-sm-2">
+							<?php if($edu_q->num_rows()>0) echo $edu->school_name ?>
                         </div>
-                        <div class="col-sm-3">
-                            <?= $row->roll_number ?>
+                        <div class="col-sm-1">
+                            <?php if($edu_q->num_rows()>0) echo $edu->passout_year ?>
                         </div>
-                        <div class="col-sm-3">
+						<div class="col-sm-3">
+                            <?php if($work_q->num_rows()>0) echo $work->company_name ?>
+                        </div>
+						<div class="col-sm-2">
+                            <?php if($work_q->num_rows()>0) echo $work->designation ?>
+                        </div>
+                        <div class="col-sm-1">
                             <a href="<?= site_url('alumni_profile/load_profile?user_id=' . $row->user_id) ?>" class="btn btn-xs btn-primary">View</a>
                         </div>
                     </div>
